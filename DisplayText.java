1ecf959c77103466d0e43b3fcc61e51d79318f0b
Ipup48 <674259022@webmail.npru.ac.th>
2024-06-28 10:53:59 +0700

    add 3 files

---
+++ DisplayText.java
@@ -0,0 +1,8 @@
+public class DisplayText {
+    public static void main(String[] args) {
+        System.out.println("Wuttichai"+ "Janmeechai");
+        System.out.println(10 + 8);
+        System.out.println("NPRU " + 2567);
+        System.out.println(0+ "2" + 2);
+    }
+}
